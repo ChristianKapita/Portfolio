9d92f52318e516427561a7bfa6b86acf17860652
chrisZip <kapitachristian2003@gmail.com>
2021-05-05 16:20:10 -0400

    added particles for animation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar"
-import Home from "./components/Home"
+import Header from "./components/Header"
 import About from "./components/About"
-import Skills from "./components/Skills"
-import Projects from "./components/Projects"
+//import Skills from "./components/Skills"
+import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import Particles from "react-particles-js";
 import './App.css'
 
 const  App = () =>{
   return (
-   <Router>
-   <div className="app">
-   <NavBar />
-   <Switch>
-   <Route exact path="/" component={Home} />
-   <Route exact path="/about" component={About} />
-   <Route exact path="/skills" component={Skills} />
-   <Route exact path="/works" component={Projects} />
-   <Route exact path="/contact" component={Contact} />
-   </Switch>
-   <Footer />
-   </div>
-   </Router>
-  //  <div className="app">
-  //    <NavBar />
-  //  </div>
-
+  <>
+<Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <NavBar />
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
